d7fdf0a06cd29cff363223356a5d8de7da470c27
ndirkx <ndirkx@3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6>
2010-12-07 19:16:33 +0000

    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/amigo_ref_interpolator@859 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- include/amigo_ref_interpolator/interpolator.h
+++ include/amigo_ref_interpolator/interpolator.h
@@ -14,13 +14,13 @@ namespace refgen {
     double x;
     double vel;
     double vel_last;
-    bool ready;
+   
 
     public:
 	  RefGenerator();
      ~RefGenerator();
     
-
+		bool ready;
       void setRefGen(double x_reset);
       amigo_msgs::ref_point generateReference(double x_desired, double max_vel, double max_acc, double dt, bool stop, double EPS);
    
